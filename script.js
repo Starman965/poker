6f87942404bf42198fc69cd68c64f3065d6612b9
Starman965 <107167236+Starman965@users.noreply.github.com>
2024-07-28 16:10:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHqMut5DC2YiBhEhMvtyX2L_5KBbKg1AU",
-  authDomain: "poker-a2e1c.firebaseapp.com",
-  databaseURL: "https://poker-a2e1c-default-rtdb.firebaseio.com",
-  projectId: "poker-a2e1c",
-  storageBucket: "poker-a2e1c.appspot.com",
-  messagingSenderId: "813172723871",
-  appId: "1:813172723871:web:8595f1cb0ffdecd4a5d2aa",
-  measurementId: "G-NSL5SLKE5H"
+    apiKey: "AIzaSyBHqMut5DC2YiBhEhMvtyX2L_5KBbKg1AU",
+    authDomain: "poker-a2e1c.firebaseapp.com",
+    databaseURL: "https://poker-a2e1c-default-rtdb.firebaseio.com",
+    projectId: "poker-a2e1c",
+    storageBucket: "poker-a2e1c.appspot.com",
+    messagingSenderId: "813172723871",
+    appId: "1:813172723871:web:8595f1cb0ffdecd4a5d2aa",
+    measurementId: "G-NSL5SLKE5H"
 };
 
 // Initialize Firebase
@@ -35,11 +35,11 @@ let schedule = [];
 // Check connection
 const connectedRef = ref(database, ".info/connected");
 onValue(connectedRef, (snap) => {
-  if (snap.val() === true) {
-    console.log("Connected to Firebase");
-  } else {
-    console.log("Not connected to Firebase");
-  }
+    if (snap.val() === true) {
+        console.log("Connected to Firebase");
+    } else {
+        console.log("Not connected to Firebase");
+    }
 });
 
 function loadDataFromFirebase() {
