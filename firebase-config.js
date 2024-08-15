6f5beca7fe53b322beaef703815e990427d3912e
David Lewis <107167236+Starman965@users.noreply.github.com>
2024-08-14 17:45:42 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBHqMut5DC2YiBhEhMvtyX2L_5KBbKg1AU",
+    authDomain: "poker-a2e1c.firebaseapp.com",
+    databaseURL: "https://poker-a2e1c-default-rtdb.firebaseio.com",
+    projectId: "poker-a2e1c",
+    storageBucket: "poker-a2e1c.appspot.com",
+    messagingSenderId: "813172723871",
+    appId: "1:813172723871:web:8595f1cb0ffdecd4a5d2aa",
+    measurementId: "G-NSL5SLKE5H"
+};
